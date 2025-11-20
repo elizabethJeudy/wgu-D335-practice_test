c57f46c7c93e6a5a2aa2daea06cfd45c588b966a
elizabethJeudy <tjeudy.e@gmail.com>
2025-11-19 22:59:57 -0500

    boolean

---
+++ lab-7.py
@@ -0,0 +1,9 @@
+# Create a program that takes an integer input and determines whether it equals one of the values in this predefined list:
+
+# predef_list = [4, -27, 15, 33, -10]
+
+# Define a function named is_in_list() that outputs a Boolean value(True or False) based on whether the input value is present in predef_list.
+
+# The solution should produce the output in the following format:
+
+# True if the input is in the list, otherwise False.
