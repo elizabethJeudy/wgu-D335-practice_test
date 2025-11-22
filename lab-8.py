4e6d2ed01516483105625a5164d62b2954a25767
elizabethJeudy <tjeudy.e@gmail.com>
2025-11-21 23:04:56 -0500

    try block

---
+++ lab-8.py
@@ -0,0 +1,11 @@
+# Create a solution that accepts one integer input representing the index value for any of the string elements in the following list:
+
+# frameworks = ["Django", "Flask", "CherryPy", "Bottle", "Web2Py", "TurboGears"]
+
+# Output the string element of the index value entered. Place the solution in a try block and implement an exception of "Error" if an incompatible integer input is provided.
+
+# The solution output should be in the format:
+
+# frameworks_element
+
+frameworks = ["Django", "Flask", "CherryPy", "Bottle", "Web2Py", "TurboGears"]
