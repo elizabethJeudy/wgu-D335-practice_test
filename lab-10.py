7cf8ac9c922276f4f7670dcdb8f51a713f8e7ff2
elizabethJeudy <tjeudy.e@gmail.com>
2025-11-23 09:09:51 -0500

    lab 10

---
+++ lab-10.py
@@ -0,0 +1,12 @@
+# Create a solution that accepts an integer input identifying how many different shares of stock are purchased from the Old Town Stock Exchange, followed by an equivalent number of string inputs representing the stock selections.
+
+# The following dictionary, stock, lists available stock selections as the key and the cost per selection as the value.
+
+# stocks = {'TSLA': 912.86, 'BBBY': 24.84, 'AAPL': 174.26, 'SOFI': 6.92,
+#        'KIRK': 8.72, 'AURA': 22.12, 'AMZN': 141.28, 'EMBK': 12.29, 'LVLU': 2.33}
+
+# Output the total cost of the purchased shares of stock to two decimal places.
+
+# The solution output should be in the format:
+
+# Total price: $cost_of_stocks
